1b676dd4d98c690aeed1378e2f557c04b4150bd8
Lucidreline <casta.ma502@gmail.com>
2021-07-17 11:12:47 -0700

    Started basic express server.

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
+const express = require('express')
 
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+app.listen(process.env.PORT, () => console.log('💻 Server ON'))
