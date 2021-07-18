90277c4336096e8fa0d9ba79367b47638a1da6d8
Lucidreline <casta.ma502@gmail.com>
2021-07-17 23:47:47 -0700

    Added to Student model.

--- models/student.js
+++ models/student.js
@@ -1,8 +1,53 @@
 const mongoose = require('mongoose')
+require('mongoose-type-email')
 
 const studentSchema = new mongoose.Schema({
-  name: String,
-  wallet: Number,
+  basicInfo: {
+    name: {
+      first: { type: String, required: true },
+      middle: String,
+      last: { type: String, required: true },
+    },
+
+    address: {
+      fullStreet: String,
+      unitNumber: String,
+      city: String,
+      state: String,
+      zipcode: String,
+    },
+
+    contactsName: {
+      first: String,
+      middle: String,
+      last: String,
+    },
+
+    parentPhoneNumber: String,
+
+    gradeLevel: Number,
+    graduationYear: Number,
+    email: { type: mongoose.SchemaTypes.Email, unique: true },
+    dateOfBirth: {
+      year: Number,
+      month: Number,
+      day: Number,
+    },
+  },
+
+  districtID: { type: String, required: true, unique: true },
+
+  clock: {
+    isClockedIn: { type: Boolean, required: true, default: false },
+    history: {
+      // history of all the times that the student has clocked in/out
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      default: [],
+    },
+  },
+
+  wallet: { type: Number, required: true, default: 0 },
 })
 
 module.exports = mongoose.model('Student', studentSchema)
